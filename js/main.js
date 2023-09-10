ac5e7a5db56713aec132f5808871e357fa5a78e4
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-09-10 21:44:46 +0300

    nextQuestion

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ const btnNext = $('.quiz__next-btn');
 let count = 0;
 let userScore = 0;
 
+btnNext.addEventListener('click', nextQuestion)
+
+
 if (typeof questions !== 'undefined' && questions.length > 0) {
     quiz.classList.remove('hidden');
     showQuetions(count);
@@ -19,7 +22,7 @@ if (typeof questions !== 'undefined' && questions.length > 0) {
 function showQuetions(index) {
     const title = $('.quiz__title');
     const list = $('.quiz__list');
-    const total = $('.quiz__footer');
+    const total = $('.quiz__total');
     let progress = $('.quiz__progress-inner');
 
     title.innerHTML = `${questions[index].question}`
@@ -33,17 +36,17 @@ function showQuetions(index) {
     const options = list.querySelectorAll('.quiz__option');
     options.forEach(el => el.setAttribute('onclick', 'optionSelected(this)'));
 
-    total.innerHTML = `${index + 1} из ${questions.length + 1}`;
+    total.innerHTML = `${index + 1} из ${questions.length}`;
     progress.style.width = `${Math.round(((index + 1) / questions.length) * 100)}%`;
 }
 
-
+// обрабатывает ответ
 function optionSelected(answer) {
     const userAnswer = answer.textContent;
     const correctAnswer = questions[count].answer;
     const options = document.querySelectorAll('.quiz__option');
-    const iconCorrect = "<span'>&#10004;</span>";
-    const iconIncorrect = "<span'>&#9940;</span>";
+    const iconCorrect = "<span>&#10004;</span>";
+    const iconIncorrect = "<span>&#9940;</span>";
 
     if (userAnswer == correctAnswer) {
         userScore += 1;
@@ -55,7 +58,6 @@ function optionSelected(answer) {
 
         options.forEach(el => {
             if (el.textContent == correctAnswer) {
-                console.log(el.textContent);
                 setTimeout(() => {
                     el.classList.add('corect');
                     el.insertAdjacentHTML('beforeend', iconCorrect)
@@ -66,5 +68,25 @@ function optionSelected(answer) {
     options.forEach(el => el.classList.add('disabled'))
 }
 
+function nextQuestion() {
+
+    const option = $('.quiz__option');
+    const result = $('.result');
+    const resultText = $('.result__text');
+
+    if ((count + 1) == questions.length && option.classList.contains('disabled')) {
+        result.classList.remove('hidden');
+        quiz.classList.add('hidden');
+        resultText.innerHTML = `Количество правильных ответов: ${userScore} из ${questions.length}`
+        return
+    }
+
+    if (option.classList.contains('disabled')) {
+        count++;
+        showQuetions(count);
+    } else {
+        alert('Выбери один из вариантов, потом переходи к следующему');
+    }
 
 
+}
